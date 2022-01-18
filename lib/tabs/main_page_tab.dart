5f30074c49b1d76feb735330bdaf613d5db42457
dordzhiev <danzansergeevich@gmail.com>
2022-01-19 02:03:03 +0300

    Some changes

--- lib/tabs/main_page_tab.dart
+++ lib/tabs/main_page_tab.dart
@@ -8,42 +8,12 @@ class MainPageTab extends StatefulWidget {
 }
 
 class _MainPageTabState extends State<MainPageTab> {
-  late TextEditingController _textController;
-  void _handleSubmitted(String text) {
-    _textController.clear();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _textController = TextEditingController();
-  }
-
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
         child: CustomScrollView(slivers: <Widget>[
       const CupertinoSliverNavigationBar(
         largeTitle: Text('Главная'),
-      ),
-      SliverToBoxAdapter(
-        child: Row(children: <Widget>[
-          Flexible(
-              child: CupertinoTextField(
-            controller: _textController,
-            onSubmitted: _handleSubmitted,
-          )),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 4.0),
-            child: CupertinoButton(
-                child: const Icon(
-                  CupertinoIcons.arrow_right_circle_fill,
-                ),
-                onPressed: () {
-                  _textController.clear();
-                }),
-          )
-        ]),
       ),
     ]));
   }
--- lib/tabs/pray_tab.dart
+++ lib/tabs/pray_tab.dart
@@ -1,15 +1,50 @@
 import 'package:flutter/cupertino.dart';
 
-class PrayTab extends StatelessWidget {
+class PrayTab extends StatefulWidget {
   const PrayTab({Key? key}) : super(key: key);
 
   @override
+  State<PrayTab> createState() => _PrayTabState();
+}
+
+class _PrayTabState extends State<PrayTab> {
+  late TextEditingController _textController;
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _textController = TextEditingController();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const CupertinoPageScaffold(
+    return CupertinoPageScaffold(
       child: CustomScrollView(
         slivers: <Widget>[
-          CupertinoSliverNavigationBar(
-            largeTitle: Text('Аккаунт'),
+          const CupertinoSliverNavigationBar(
+            largeTitle: Text('Молитва'),
+          ),
+          SliverToBoxAdapter(
+            child: Row(children: <Widget>[
+              Flexible(
+                  child: CupertinoTextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+              )),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                child: CupertinoButton(
+                    child: const Icon(
+                      CupertinoIcons.arrow_right_circle_fill,
+                    ),
+                    onPressed: () {
+                      _textController.clear();
+                    }),
+              )
+            ]),
           ),
         ],
       ),
